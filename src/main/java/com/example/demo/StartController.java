220e4a618cd241ee2307aa2f68a9e57d556e6142
Tom Delalande <tom.delalande@etu.umontpellier.fr>
2021-10-04 10:34:07 +0200

    debut vue start view

--- src/main/java/com/example/demo/StartController.java
+++ src/main/java/com/example/demo/StartController.java
@@ -3,7 +3,7 @@ package com.example.demo;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class StartController {
     @FXML
     private Label welcomeText;
 
