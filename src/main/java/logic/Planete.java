5815ccfbc7ed18ab0f776203bf244dca27c99b9e
Liam Thorel <72860186+ToasterLiam@users.noreply.github.com>
2021-10-09 10:37:56 +0200

    Ajout arguments position sur les planètes

--- src/main/java/logic/Planete.java
+++ src/main/java/logic/Planete.java
@@ -4,11 +4,15 @@ public class Planete extends Astre{
     private float taille;
     private float masse;
     private String nom;
+    private int positionX;
+    private int positionY;
 
-    public Planete(String nom, float taille, float masse ) {
+    public Planete(String nom, float taille, float masse, int pX, int pY ) {
         this.taille = taille;
         this.masse = masse;
         this.nom = nom;
+        this.positionX = pX;
+        this.positionY = pY;
     }
 
     @Override
