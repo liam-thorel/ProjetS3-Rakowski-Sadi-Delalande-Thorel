3d413fe614ac0d31ecd2124d0c033b9f6e7f9fe9
Tom Delalande <tom.delalande@etu.umontpellier.fr>
2021-10-20 13:19:56 +0200

    Divsion des Menu en différents Menu

--- src/main/java/view/Menu.java
+++ src/main/java/view/Menu.java
@@ -19,9 +19,9 @@ public class Menu extends Pane {
 
     private VBox menuEtChangeMenu;
     private BorderPane changeMenu;
-    private HBox menu;
-    private HBox menuAstre;
-    private HBox addAstre;
+    private Pane menuAjouter;
+    private Pane menuActuel;
+    private Pane menuSysteme;
     private HBox systemeOuAjouter;
     private HBox playOuPause;
     private Button systeme;
@@ -29,21 +29,16 @@ public class Menu extends Pane {
     private Button play;
     private Button pause;
     private SimulationView s;
-    private Pane menuBg;
-    private Pane menuAstreBg;
-private Pane menuAddAstreBg;
 
     public Menu(SimulationView s) {
         this.s=s;
         setWidth(1500);
         setHeight(200);
         changeMenu = new BorderPane();
-        menu = new HBox();
-        menuAstreBg = new Pane();
-        menuAddAstreBg = new Pane();
+        menuAjouter = new MenuAjouter().getMenuBg();
+        menuSysteme = new Pane();
+        menuActuel = menuAjouter;
         menuEtChangeMenu = new VBox();
-        menuAstre = new HBox();
-        addAstre = new HBox();
         systemeOuAjouter = new HBox();
         playOuPause = new HBox();
         systeme = new Button("Systeme");
@@ -58,42 +53,14 @@ private Pane menuAddAstreBg;
         pause = new Button("Pause");
         pause.prefWidth(20);
         pause.prefHeight(15);
-        menuBg = new Pane();
 
-        //Création et affectation du réctangle d'arrière plan derrière le menu
-        Rectangle rectangle = new Rectangle();
-        rectangle.setWidth(1485);
-        rectangle.setHeight(350);
-        rectangle.setArcWidth(20);
-        rectangle.setArcHeight(20);
-        rectangle.setFill(Color.rgb(190, 190, 190, 0.9));
-        menuBg.getChildren().add(rectangle);
-        menuBg.getChildren().add(menu);
-
-        //Création et éffectation du réctangle d'arrière plan derrière Mes astres
-        Rectangle rectangleMenuAstres = new Rectangle();
-        rectangleMenuAstres.setHeight(70);
-        rectangleMenuAstres.setWidth(525);
-        rectangleMenuAstres.setFill(Color.rgb(220,220,190,1));
-        menuAstreBg.getChildren().add(rectangleMenuAstres);
-        menuAstreBg.getChildren().add(menuAstre);
-
-        //Création et éffectation du réctangle d'arrière plan derrière Mes astres
-        Rectangle rectangleAddAstres = new Rectangle();
-        rectangleAddAstres.setHeight(70);
-        rectangleAddAstres.setWidth(525);
-        rectangleAddAstres.setFill(Color.rgb(220,220,190,1));
-        menuAddAstreBg.getChildren().add(rectangleAddAstres);
-        menuAddAstreBg.getChildren().add(addAstre);
-
-        //eloignement des différents Menu
-        menu.setSpacing(380);
-        menu.setPadding(new Insets(25));
-
+        // Que font les boutons ?
         systeme.setOnAction(onSystemeMenu);
         ajouter.setOnAction(onAjouterMenu);
         play.setOnAction(onPlay);
         pause.setOnAction(onPause);
+
+        //
         systemeOuAjouter.getChildren().addAll(systeme, ajouter);
         changeMenu.setLeft(systemeOuAjouter);
         changeMenu.setRight(playOuPause);
@@ -101,8 +68,7 @@ private Pane menuAddAstreBg;
         playOuPause.setSpacing(5);
         changeMenu.setMaxWidth(1480);
         playOuPause.getChildren().addAll(play,pause);
-        menu.getChildren().addAll(menuAstreBg, menuAddAstreBg);
-        menuEtChangeMenu.getChildren().addAll(changeMenu, menuBg);
+        menuEtChangeMenu.getChildren().addAll(changeMenu,menuActuel);
 
 
 
@@ -111,14 +77,18 @@ private Pane menuAddAstreBg;
         private EventHandler<ActionEvent> onAjouterMenu = new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent actionEvent){
-                System.out.println("je fais rien je suis deja en vue ajouter");
+                menuEtChangeMenu.getChildren().remove(menuActuel);
+                menuActuel=menuAjouter;
+                menuEtChangeMenu.getChildren().add(menuActuel);
             }
         };
 
     private EventHandler<ActionEvent> onSystemeMenu = new EventHandler<ActionEvent>() {
         @Override
         public void handle(ActionEvent actionEvent){
-            System.out.println("j'affiche la vue du menu systeme");
+            menuEtChangeMenu.getChildren().remove(menuActuel);
+            menuActuel=menuSysteme;
+            menuEtChangeMenu.getChildren().add(menuActuel);
         }
     };
 
