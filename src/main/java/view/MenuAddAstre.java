ae6a85d12e7d11d2f46b8926fee7785433306d50
Tom Delalande <tom.delalande@etu.umontpellier.fr>
2021-10-27 14:25:52 +0200

    Label planète

--- src/main/java/view/MenuAddAstre.java
+++ src/main/java/view/MenuAddAstre.java
@@ -1,18 +1,25 @@
 package view;
 
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
 public class MenuAddAstre extends HBox {
     private Button newAstre;
     private TextField nom, taille, masse, positionX, positionY, vitesseX, vitesseY, estFixe;
+    private Label nomtxt, tailletxt, massetxt, positionXtxt, positionYtxt, vitesseXtxt, vitesseYtxt, estFixetxt;
 
     public MenuAddAstre(MenuAjouter mA) {
         newAstre = new Button();
 
         //contenu de Add Astre
         nom = new TextField();
+        nomtxt = new Label("nom");
+        VBox nameAll = new VBox();
+        nameAll.getChildren().addAll(nomtxt,nom);
+
         taille = new TextField();
         masse = new TextField();
         positionX = new TextField();
@@ -20,7 +27,7 @@ public class MenuAddAstre extends HBox {
         vitesseX = new TextField();
         vitesseY = new TextField();
         estFixe = new TextField();
-        getChildren().addAll(nom, taille, masse, positionX, positionY, vitesseX, vitesseY, estFixe);
+        getChildren().addAll(nameAll, taille, masse, positionX, positionY, vitesseX, vitesseY, estFixe);
         newAstre.setText("Ajouter Astre");
         getChildren().add(newAstre);
     }
