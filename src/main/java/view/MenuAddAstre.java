f51fc2167722fbd1c856769fdf2b6878f3bb0cf1
Maëlis-Rakowski <maelis.rakowski@etu.umontpellier.fr>
2021-10-27 14:07:49 +0200

    debut du menu addAstre

--- src/main/java/view/MenuAddAstre.java
+++ src/main/java/view/MenuAddAstre.java
@@ -1,7 +1,27 @@
 package view;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 
 public class MenuAddAstre extends HBox {
-    //a compléter
+    private Button newAstre;
+    private TextField nom, taille, masse, positionX, positionY, vitesseX, vitesseY, estFixe;
+
+    public MenuAddAstre(MenuAjouter mA) {
+        newAstre = new Button();
+
+        //contenu de Add Astre
+        nom = new TextField();
+        taille = new TextField();
+        masse = new TextField();
+        positionX = new TextField();
+        positionY = new TextField();
+        vitesseX = new TextField();
+        vitesseY = new TextField();
+        estFixe = new TextField();
+        getChildren().addAll(nom, taille, masse, positionX, positionY, vitesseX, vitesseY, estFixe);
+        newAstre.setText("Ajouter Astre");
+        getChildren().add(newAstre);
+    }
 }
