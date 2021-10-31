e388b35f8a912566ec59e1b299b609e1dc19e09d
Tom Delalande <tom.delalande@etu.umontpellier.fr>
2021-10-31 11:03:26 +0100

    Label planètes

--- src/main/java/view/MenuAddAstre.java
+++ src/main/java/view/MenuAddAstre.java
@@ -16,18 +16,47 @@ public class MenuAddAstre extends HBox {
 
         //contenu de Add Astre
         nom = new TextField();
-        nomtxt = new Label("nom");
+        nomtxt = new Label("Nom");
         VBox nameAll = new VBox();
         nameAll.getChildren().addAll(nomtxt,nom);
 
         taille = new TextField();
+        tailletxt = new Label("Taille");
+        VBox tailleAll = new VBox();
+        tailleAll.getChildren().addAll(tailletxt,taille);
+
         masse = new TextField();
+        massetxt = new Label("Masse");
+        VBox masseAll = new VBox();
+        masseAll.getChildren().addAll(massetxt,masse);
+
         positionX = new TextField();
+        positionXtxt = new Label("Position X");
+        VBox positionXAll = new VBox();
+        positionXAll.getChildren().addAll(positionXtxt,positionX);
+
         positionY = new TextField();
+        positionYtxt = new Label("Position Y");
+        VBox positionYAll = new VBox();
+        positionYAll.getChildren().addAll(positionYtxt,positionY);
+
         vitesseX = new TextField();
+        vitesseXtxt = new Label("Vitesse X");
+        VBox vitesseXAll = new VBox();
+        vitesseXAll.getChildren().addAll(vitesseXtxt,vitesseX);
+
         vitesseY = new TextField();
+        vitesseYtxt = new Label("Vitesse Y");
+        VBox vitesseYAll = new VBox();
+        vitesseYAll.getChildren().addAll(vitesseYtxt,vitesseY);
+
         estFixe = new TextField();
-        getChildren().addAll(nameAll, taille, masse, positionX, positionY, vitesseX, vitesseY, estFixe);
+        estFixetxt = new Label("fixe ?");
+        VBox estFixeAll = new VBox();
+        estFixeAll.getChildren().addAll(estFixetxt,estFixe);
+
+        estFixe = new TextField();
+        getChildren().addAll(nameAll, tailleAll, masseAll, positionXAll, positionYAll, vitesseXAll, vitesseYAll, estFixeAll);
         newAstre.setText("Ajouter Astre");
         getChildren().add(newAstre);
     }
