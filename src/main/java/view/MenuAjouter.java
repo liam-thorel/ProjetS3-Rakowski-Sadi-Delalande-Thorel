6fbcd1171b538eeceba55c9ae3512037571ab7e0
sadi.leina@outlook.fr <sadi.leina@outlook.fr>
2021-11-16 17:51:31 +0100

    suite drag n drop

--- src/main/java/view/MenuAjouter.java
+++ src/main/java/view/MenuAjouter.java
@@ -6,10 +6,16 @@ import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.geometry.Orientation;
 import javafx.scene.control.ListView;
+import javafx.scene.image.WritableImage;
+import javafx.scene.input.ClipboardContent;
+import javafx.scene.input.DataFormat;
+import javafx.scene.input.Dragboard;
+import javafx.scene.input.TransferMode;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
 
@@ -69,31 +75,59 @@ public class MenuAjouter extends Pane {
                 }
             }
         });
+        ///// DRAG N' DROP /////
+        mesAstres.setOnDragDetected(mouseEvent -> {
+            System.out.println("Drag n' Drop detecté mon ptit pote");
+            final Dragboard dragBroard = mesAstres.startDragAndDrop(TransferMode.MOVE);
+            // Remlissage du contenu.
+            ClipboardContent content = new ClipboardContent();
+            /*
+            // Exporter en tant que texte.
+            content.putString("Planète");*/
+            // Exporter en tant que couleur ARGB.
+            DataFormat paintFormat = DataFormat.lookupMimeType(Paint.class.getName());
+            paintFormat = (paintFormat == null) ? new DataFormat(Paint.class.getName()) : paintFormat;
+            Color color = Color.BLUE;//mesAstres.getSelectionModel().getSelectedIndex().getFill();
+            int red = (int) (255 * color.getRed());
+            int green = (int) (255 * color.getGreen());
+            int blue = (int) (255 * color.getBlue());
+            int alpha = (int) (255 * color.getOpacity());
+            int argb = alpha << 24 | red << 16 | green << 8 | blue << 0;
+            content.put(paintFormat, argb);
+            // Exporter en tant qu'image.
+            WritableImage capturePlanete = mesAstres.snapshot(null, null);
+            content.putImage(capturePlanete);
+            //
+            dragBroard.setContent(content);
+            mouseEvent.consume();
+        });
 
-        //Création et éffectation du réctangle d'arrière plan derrière Mes astres
-        Rectangle rectangleMenuAstres = new Rectangle();
-        rectangleMenuAstres.setHeight(70);
-        rectangleMenuAstres.setWidth(695);
-        rectangleMenuAstres.setFill(Color.rgb(64,68,75,1));
-        menuAstreBg.getChildren().add(rectangleMenuAstres);
-        menuAstreBg.getChildren().add(menuAstre);
+            //Création et éffectation du réctangle d'arrière plan derrière Mes astres
+            Rectangle rectangleMenuAstres = new Rectangle();
+            rectangleMenuAstres.setHeight(70);
+            rectangleMenuAstres.setWidth(695);
+            rectangleMenuAstres.setFill(Color.rgb(64, 68, 75, 1));
+            menuAstreBg.getChildren().add(rectangleMenuAstres);
+            menuAstreBg.getChildren().add(menuAstre);
 
-        //Création et éffectation du réctangle d'arrière plan derrière Add astres
-        Rectangle rectangleAddAstres = new Rectangle();
-        rectangleAddAstres.setHeight(70);
-        rectangleAddAstres.setWidth(695);
-        rectangleAddAstres.setFill(Color.rgb(64,68,75,1));
+            //Création et éffectation du réctangle d'arrière plan derrière Add astres
+            Rectangle rectangleAddAstres = new Rectangle();
+            rectangleAddAstres.setHeight(70);
+            rectangleAddAstres.setWidth(695);
+            rectangleAddAstres.setFill(Color.rgb(64, 68, 75, 1));
 
-        menuAddAstreBg.getChildren().add(rectangleAddAstres);
-        menuAddAstreBg.getChildren().add(addAstre);
+            menuAddAstreBg.getChildren().add(rectangleAddAstres);
+            menuAddAstreBg.getChildren().add(addAstre);
 
 
-        //eloignement des différents Menu
-        menu.setSpacing(100);
-        menu.setPadding(new Insets(25));
+            //eloignement des différents Menu
+            menu.setSpacing(100);
+            menu.setPadding(new Insets(25));
 
-        //ajout a menu de l'ensemble des sous menu
-        menu.getChildren().addAll(menuAstreBg, menuAddAstreBg);
+            //ajout a menu de l'ensemble des sous menu
+            menu.getChildren().addAll(menuAstreBg, menuAddAstreBg);
+
+
 
     }
 
