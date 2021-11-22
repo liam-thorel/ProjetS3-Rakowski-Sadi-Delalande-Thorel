21e1da26a46ba8fde768f993910b98994c978a20
Tom Delalande <tom.delalande@etu.umontpellier.fr>
2021-11-22 12:27:52 +0100

    refonte graphique chargeViewFile

--- src/main/java/view/ChooseFileView.java
+++ src/main/java/view/ChooseFileView.java
@@ -2,10 +2,12 @@ package view;
 
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import logic.Simulation;
@@ -18,14 +20,14 @@ public class ChooseFileView extends Stage {
 
     private Button chargerFile, chargerSystemeSolaire, retourPageAccueil;
     private PlaneteApp app;
-    private BorderPane root;
+    private Pane root;
     public File fichierSimu;
     private VBox boutons;
     private Label erreur;
 
     public ChooseFileView(PlaneteApp app) {
         this.app = app;
-        root = new BorderPane();
+        root = new Pane();
         boutons = new VBox();
 
         chargerSystemeSolaire = new Button("charger Système Solaire");
@@ -36,11 +38,14 @@ public class ChooseFileView extends Stage {
 
         chargerFile = new Button("charger simulation");
         chargerFile.setOnAction(chargerFileEvent);
-        boutons.getChildren().add(chargerFile);
 
-        root.setBottom(retourPageAccueil);
-        root.setCenter(boutons);
-        root.setTop(chargerSystemeSolaire);
+        boutons.relocate(app.getStage().getWidth()/2+300,app.getStage().getWidth()/2);
+
+        boutons.getChildren().addAll(chargerSystemeSolaire,chargerFile,retourPageAccueil);
+        boutons.setSpacing(50);
+        boutons.setAlignment(Pos.CENTER);
+
+        root.getChildren().add(boutons);
         root.setId("bg");
         root.getStylesheets().add(this.getClass().getResource("/Css/simulation.css").toExternalForm());
 
