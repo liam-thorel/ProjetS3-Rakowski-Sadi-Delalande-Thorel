f02b16d695f1835c9c107c9787586d37b733d4d7
Tom Delalande <tom.delalande@etu.umontpellier.fr>
2021-11-22 14:01:02 +0100

    beau bouttons

--- src/main/java/view/ChooseFileView.java
+++ src/main/java/view/ChooseFileView.java
@@ -9,6 +9,7 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import logic.Simulation;
 
@@ -30,16 +31,23 @@ public class ChooseFileView extends Stage {
         root = new Pane();
         boutons = new VBox();
 
-        chargerSystemeSolaire = new Button("charger Système Solaire");
+        chargerSystemeSolaire = new Button("Charger Système Solaire");
         chargerSystemeSolaire.setOnAction(chargerSystemeSol);
+        chargerSystemeSolaire.setStyle("-fx-font: 35 arial;-fx-font-family: OpenSymbol;-fx-font-weight: bold;");
+        chargerSystemeSolaire.setPrefSize(500,70);
 
         retourPageAccueil = new Button("Retour à la Page d'accueil");
         retourPageAccueil.setOnAction(pageAccueil);
+        retourPageAccueil.setStyle("-fx-font: 35 arial;-fx-font-family: OpenSymbol;-fx-font-weight: bold;");
+        retourPageAccueil.setPrefSize(500,70);
 
-        chargerFile = new Button("charger simulation");
+        chargerFile = new Button("Charger simulation");
         chargerFile.setOnAction(chargerFileEvent);
+        chargerFile.setStyle("-fx-font: 35 arial;-fx-font-family: OpenSymbol;-fx-font-weight: bold;");
+        chargerFile.setPrefSize(500,70);
 
-        boutons.relocate(app.getStage().getWidth()/2+300,app.getStage().getWidth()/2);
+
+        boutons.relocate(app.getStage().getWidth()/2+160,app.getStage().getWidth()/2-75);
 
         boutons.getChildren().addAll(chargerSystemeSolaire,chargerFile,retourPageAccueil);
         boutons.setSpacing(50);
