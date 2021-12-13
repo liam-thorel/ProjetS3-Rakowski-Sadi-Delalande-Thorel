fcba575bc55146ae4e395478a531d851ff5821a1
Delalande-Tom <tom.delalande@etu.umontpellier.fr>
2021-12-13 14:49:18 +0100

    début crédits

--- src/main/java/view/CreditView.java
+++ src/main/java/view/CreditView.java
@@ -3,31 +3,35 @@ package view;
 
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.Border;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 
 public class CreditView extends Stage {
-    private Pane root;
-    private BorderPane all;
+    private BorderPane root;
     private Button retourPageAccueil;
     private Label remerciement;
     private Label tuteur;
     private Label createur;
     private Label createurs;
     private PlaneteApp app;
+    private VBox text;
+    private HBox centrage;
 
     public CreditView(PlaneteApp app){
         this.app=app;
-        root = new Pane();
-        all = new BorderPane();
+        root = new BorderPane();
+        text = new VBox();
+        centrage = new HBox();
+        remerciement = new Label("Remerciments :");
         retourPageAccueil = new Button("Retour à la Page d'accueil");
 
         root.setId("bg");
@@ -37,10 +41,15 @@ public class CreditView extends Stage {
 
         retourPageAccueil.setOnAction(pageAccueil);
         retourPageAccueil.setStyle("-fx-font: 30 arial;-fx-font-family: OpenSymbol;-fx-font-weight: bold;");
+        centrage.setAlignment(Pos.CENTER);
+        centrage.getChildren().add(retourPageAccueil);
+        centrage.setPadding(new Insets(0,0,50,0));
 
-        all.setBottom(retourPageAccueil);
 
-        root.getChildren().add(all);
+
+        text.getChildren().add(remerciement);
+        root.setCenter(text);
+        root.setBottom(centrage);
 
         Scene scene = new Scene(root);
         app.getStage().setScene(scene);
