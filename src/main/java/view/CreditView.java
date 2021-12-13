cf74518a6a712145d92f883869936f933bd63d07
Delalande-Tom <tom.delalande@etu.umontpellier.fr>
2021-12-13 14:29:14 +0100

    les prémices des crédits

--- src/main/java/view/CreditView.java
+++ src/main/java/view/CreditView.java
@@ -1,9 +1,13 @@
 package view;
 
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -11,24 +15,40 @@ import javafx.stage.Stage;
 
 public class CreditView extends Stage {
     private Pane root;
-    private VBox all;
-    private Button retourEcranAccueil;
+    private BorderPane all;
+    private Button retourPageAccueil;
     private Label remerciement;
     private Label tuteur;
     private Label createur;
     private Label createurs;
+    private PlaneteApp app;
 
     public CreditView(PlaneteApp app){
+        this.app=app;
         root = new Pane();
-        Label test = new Label("TEST");
+        all = new BorderPane();
 
         root.setId("bg");
         root.getStylesheets().add(this.getClass().getResource("/Css/simulation.css").toExternalForm());
 
-        root.getChildren().add(test);
+        retourPageAccueil = new Button("Retour à la Page d'accueil");
+        retourPageAccueil.setOnAction(pageAccueil);
+        retourPageAccueil.setStyle("-fx-font: 35 arial;-fx-font-family: OpenSymbol;-fx-font-weight: bold;");
+        retourPageAccueil.setPrefSize(500,70);
+
+        all.setBottom(retourPageAccueil);
+
+        root.getChildren().add(all);
 
         Scene scene = new Scene(root);
         app.getStage().setScene(scene);
     }
+
+    private EventHandler<ActionEvent> pageAccueil = new EventHandler<ActionEvent>() {
+        @Override
+        public void handle(ActionEvent actionEvent) {
+            app.initStart();
+        }
+    };
 
 }
