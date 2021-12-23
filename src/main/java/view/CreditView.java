e16bc93a8a5fe4bf6f20080cbc47bca0750d07c5
Tom Delalande <tom.delalande@etu.umontpellier.fr>
2021-12-23 20:15:12 +0100

    CreditView Comment

--- src/main/java/view/CreditView.java
+++ src/main/java/view/CreditView.java
@@ -10,7 +10,6 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
